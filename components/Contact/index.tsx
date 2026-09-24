5f38d3ffbe31ba6d9251c42760a4485d543ed303
akbar <akbarnasa5@gmail.com>
2026-09-24 11:45:29 +0700

    fixed: testimonial pelanggan

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -145,14 +145,14 @@ const Contact = () => {
               className="animate_top w-full md:w-2/5 md:p-7.5 lg:w-[26%] xl:pt-15"
             >
               <h2 className="mb-12.5 text-3xl font-semibold text-black dark:text-white xl:text-sectiontitle2">
-                Temukan Kita
+                Temukan Kami
               </h2>
 
               <div className="5 mb-7">
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">
-                  Lokasi Kita
+                  Lokasi Kami
                 </h3>
-                <p>290 Maryam Springs 260, Courbevoie, Paris, France</p>
+                <p>Bandung, Jawa Barat 40376</p>
               </div>
               <div className="5 mb-7">
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">
@@ -167,7 +167,7 @@ const Contact = () => {
                   Nomor Telepon
                 </h4>
                 <p>
-                  <a href="#">+009 42334 6343 843</a>
+                  <a href="#">+628 99999999</a>
                 </p>
               </div>
             </motion.div>
